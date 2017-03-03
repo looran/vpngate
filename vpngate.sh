a97a87f6f1da99b63d95b2823efa026063cb90ef
Laurent Ghigonis <laurent@gouloum.fr>
2017-03-03 22:08:16 +0200

    more informations in file names, and display naming

--- vpngate.sh
+++ vpngate.sh
@@ -25,13 +25,12 @@ helpexit() {
 
 	list     : list vpngate .ovpn files
 	list all : show all gates instead of best per country/protocol (last is best)
-	refresh  : refresh from vpngate.net
+	refresh  : download from vpngate.net
 	fav      : list favorites
 	fav <file.ovpn> [<file2.ovpn...]          : add one or more files to favorites
 	fav rm <file.ovpn> [<file2.ovpn...] | all : remove one or more files from favorites
 
-	.ovpn files are named after "vpngate_SCORE_IP_PROTO_COUNTRY.ovpn"
-	refreshed files are in $LIST/
+	dowloaded files are in $LIST/
 	favorite  files are in $FAV/
 	_EOF
 	exit
@@ -45,17 +44,29 @@ do_refresh() {
 	tail -n+3 $LISTFILE |head -n-1 |while read s; do
 		ip=$(echo $s |cut -d',' -f2)
 		score=$(echo $s |cut -d',' -f3)
+		ping=$(echo $s |cut -d',' -f4)
+		speed=$(printf "%.2f\n" $(bc -l <<< "$(echo $s |cut -d',' -f5) / 1000000"))
 		country=$(echo $s |cut -d',' -f6)
+		sessions=$(echo $s |cut -d',' -f8)
+		uptime=$(echo $s |cut -d',' -f9)
+		totalusers=$(echo $s |cut -d',' -f10)
+		totaltraffic=$(printf "%.2f\n" $(bc -l <<< "$(echo $s |cut -d',' -f11) / 1000000"))
 		echo $s |cut -d',' -f15 |head -n6 |tail -n1 |base64 -d > $LIST/tmp 2>/dev/null
 		proto="$(echo $(egrep "^proto tcp" $LIST/tmp ||echo "proto udp") |cut -d' ' -f2 |tr -d '\r' |tr '[a-z]' '[A-Z]')"
-		mv $LIST/tmp "$LIST/vpngate_${score}_${ip}_${proto}_${country}.ovpn"
+		mv $LIST/tmp "$LIST/vpngate_${score}_${ip}_${proto}_${country}_${ping}_${speed}_${totalusers}_${totaltraffic}_${uptime}.ovpn"
 	done
 }
 do_list() {
+	echo "#${LIST}1       2     3  4     5       6    7     8          9            10"
+	echo "#${LIST}vpngate_score_ip_proto_country_ping_speed_totalusers_totaltraffic_uptime"
+	echo "#"
 	find $LIST -name "vpngate_*" |sort -n -t'_' -k2 -r |sort -t'_' -k4,5 -u |sort -n -t'_' -k2
 	do_fav_list
 }
 do_list_all() {
+	echo "#${LIST}1       2     3  4     5       6    7     8          9            10"
+	echo "#${LIST}vpngate_score_ip_proto_country_ping_speed_totalusers_totaltraffic_uptime"
+	echo "#"
 	find $LIST -name "vpngate_*" |sort -n -t'_' -k2
 	do_fav_list
 }
